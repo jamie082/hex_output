6e253470c546c798479449014d9a52068cf708a7
jamie082 <jamiemorrissey02@gmail.com>
2021-08-09 03:47:13 +0000

    updated source

--- hex_algorithm.c
+++ hex_algorithm.c
@@ -10,7 +10,6 @@
 
 int main (void)
 {
-	
 	int p = 0;
 
 	char alphabet[LIMIT_2] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'w', 'y', 'z', '\0' };
@@ -19,18 +18,34 @@ int main (void)
 	int k_2 = 61;
 	int x = 97;
 	int u = 0;
+	int ppp = 1;
+	char *asterisk = "*";
 
-	while (p <= 26 && k_2 <= 79 && x <= 123 && p <= LIMIT_2) // k_2 <= 79	
+	while (p <= 26 && k_2 <= 88 && x <= 123 && p <= LIMIT_2 && ppp <= 6) // k_2 <= 79	
 	{
-		printf("\n%c\t%d", alphabet[p], x);
+		printf("\n%c\t", alphabet[p]);
+		printf("%d", x);
+
+		if (x == 105)
+		{
+			int n = 1;
+			while (p <= 6 && n < 4)
+			{
+				printf("%s\n", asterisk);
+				n++;
+			}
+		}
+
 		printf("\t%d", k_2);	
 
 		if (k_2 == 70)
 		{
-			while (u <= 5)
+			int f = 1;
+
+			while (f <= 6 && u <= 5)
 			{
 				printf("\n");
-				printf("\t\t6%c", array[u]);
+				printf("%d\t\t6%c", f++, array[u]);
 				u++;
 			}
 		}
